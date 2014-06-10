440a614c9e547789aa053dca7c44e5051dd1d661
Wells Santo <theonlydevking@gmail.com>
2014-06-09 23:55:54 -0400

    Added functionality to /upload to display POST data

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,10 +22,11 @@ function start(response) {
 function texter(response) {
 	console.log("Request handler for the text field function was called.");
 
+	// Wouldn't have it inline like this for MVC
 	var body = '<html>' + '<head>' + '<title>Text Field Thing</title>' + '</head>'
 	+ '<body>' + '<h1>Some Text Field Thing</h1>' + '<form action="/upload" method="post">' // recall that how forms work, this sends them to the '/upload' page after
 	+ '<textarea name="text" rows="20" cols="60"></textarea>' +
-	'<input type="submit" value="Submit text" />' +
+	'<br /><input type="submit" value="Submit text" />' +
 	'</form>' + '</body>' + '</html>';
 
 	response.writeHead(200, {"Content-Type": "text/html"});
@@ -34,11 +35,13 @@ function texter(response) {
 
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	// Just basic on-the-page printing
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello upload ... HTML tags won't work in here because content type is plain text!");
+	
+	response.write("\nYou've also sent the data: " + postData);
 	response.end();
 }
 
