100d457d72723c4fb984a301a128d49be4df533a
Wells Santo <theonlydevking@gmail.com>
2014-06-27 20:36:52 -0400

    First time using vim!

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var server = require("./server");	// Forces server.js to be loaded
 var router = require("./router"); 	// Forces route.js to be loaded
 var requestHandlers = require("./requestHandlers"); // Forces requestHandler.js to be loaded
-
-var handle = {}	// Defining a variable array / association map
+// Testing use of vim in terminal...
 // We MUST list the association mapping here in order to relate the URL to a function
 handle["/"] = requestHandlers.start; // Define as 'start' method given by mapped string
 handle["/start"] = requestHandlers.start;
